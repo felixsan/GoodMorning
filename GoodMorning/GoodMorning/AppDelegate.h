026d70a010e025cadec7b4937e0e77c6ff9c2c5c
Felix Santiago <fsantiago@hearsaycorp.com>
2013-10-17 19:48:08 -0700

    Initial commit with wireframes

---
+++ GoodMorning/GoodMorning/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GoodMorning
+//
+//  Created by Felix Santiago on 10/17/13.
+//  Copyright (c) 2013 MakeItRain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
