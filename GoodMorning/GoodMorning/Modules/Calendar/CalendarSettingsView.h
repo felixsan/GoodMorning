bdaa409e162a91974054664b1e90602fc8ff1849
Felix Santiago <fsantiago@hearsaycorp.com>
2013-11-01 18:48:49 -0700

    Adding the calendar module files

---
+++ GoodMorning/GoodMorning/Modules/Calendar/CalendarSettingsView.h
@@ -0,0 +1,13 @@
+//
+//  CalendarSettingsView.h
+//  GoodMorning
+//
+//  Created by Felix Santiago on 11/1/13.
+//  Copyright (c) 2013 MakeItRain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalendarSettingsView : UIView
+
+@end
---
+++ GoodMorning/GoodMorning/Modules/Calendar/CalendarSettingsViewController.m
@@ -0,0 +1,38 @@
+//
+//  CalendarSettingsViewController.m
+//  GoodMorning
+//
+//  Created by Felix Santiago on 11/1/13.
+//  Copyright (c) 2013 MakeItRain. All rights reserved.
+//
+
+#import "CalendarSettingsViewController.h"
+
+@interface CalendarSettingsViewController ()
+
+@end
+
+@implementation CalendarSettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
