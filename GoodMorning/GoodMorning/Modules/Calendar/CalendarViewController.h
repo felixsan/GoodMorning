a4d4475fbd1394147548f348b5718c73fe0bcf6d
Felix Santiago <fsantiago@hearsaycorp.com>
2013-11-01 19:27:13 -0700

    Display Calendar events for the next 24 hours on the table

--- GoodMorning/GoodMorning/Modules/Calendar/CalendarViewController.h
+++ GoodMorning/GoodMorning/Modules/Calendar/CalendarViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CalendarViewController : UIViewController
+@interface CalendarViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
 
 @end
--- GoodMorning/GoodMorning/Modules/Calendar/CalendarViewController.m
+++ GoodMorning/GoodMorning/Modules/Calendar/CalendarViewController.m
@@ -6,9 +6,25 @@
 //  Copyright (c) 2013 MakeItRain. All rights reserved.
 //
 
+#import <EventKit/EventKit.h>
+#import <EventKitUI/EventKitUI.h>
 #import "CalendarViewController.h"
 
-@interface CalendarViewController ()
+@interface CalendarViewController () <EKEventEditViewDelegate>
+
+// EKEventStore instance associated with the current Calendar application
+@property (nonatomic, strong) EKEventStore *eventStore;
+
+// Default calendar associated with the above event store
+@property (nonatomic, strong) EKCalendar *defaultCalendar;
+
+// Array of all events happening within the next 24 hours
+@property (nonatomic, strong) NSMutableArray *eventsList;
+
+// Used to add events to Calendar
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *addButton;
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -26,13 +42,178 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    // Initialize the event store
+    self.eventStore = [[EKEventStore alloc] init];
+    // Initialize the events list
+    self.eventsList = [[NSMutableArray alloc] initWithCapacity:0];
+    // The Add button is initially disabled
+    self.addButton.enabled = NO;
+
+    // Register our cell
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"eventCell"];
+
+    // Check whether we are authorized to access Reminders
+    [self checkEventStoreAccessForCalendar];
 }
 
-- (void)didReceiveMemoryWarning
+
+#pragma mark -
+#pragma mark Table View
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    return self.eventsList.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"eventCell" forIndexPath:indexPath];
+
+    // Get the event at the row selected and display its title
+    cell.textLabel.text = [[self.eventsList objectAtIndex:(NSUInteger) indexPath.row] title];
+    return cell;
+}
+
+
+#pragma mark -
+#pragma mark Access Calendar
+
+// Check the authorization status of our application for Calendar
+-(void)checkEventStoreAccessForCalendar
+{
+    EKAuthorizationStatus status = [EKEventStore authorizationStatusForEntityType:EKEntityTypeEvent];
+
+    switch (status)
+    {
+        // Update our UI if the user has granted access to their Calendar
+        case EKAuthorizationStatusAuthorized: [self accessGrantedForCalendar];
+            break;
+            // Prompt the user for access to Calendar if there is no definitive answer
+        case EKAuthorizationStatusNotDetermined: [self requestCalendarAccess];
+            break;
+            // Display a message if the user has denied or restricted access to Calendar
+        case EKAuthorizationStatusDenied:
+        case EKAuthorizationStatusRestricted:
+        {
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Privacy Warning" message:@"Permission was not granted for Calendar"
+                                                           delegate:nil
+                                                  cancelButtonTitle:@"OK"
+                                                  otherButtonTitles:nil];
+            [alert show];
+        }
+            break;
+        default:
+            break;
+    }
+}
+
+
+// Prompt the user for access to their Calendar
+-(void)requestCalendarAccess
+{
+    [self.eventStore requestAccessToEntityType:EKEntityTypeEvent completion:^(BOOL granted, NSError *error)
+    {
+        if (granted)
+        {
+            // Let's ensure that our code will be executed from the main queue
+            dispatch_async(dispatch_get_main_queue(), ^{
+                // The user has granted access to their Calendar; let's populate our UI with all events occuring in the next 24 hours.
+                [self accessGrantedForCalendar];
+            });
+        }
+    }];
+}
+
+
+// This method is called when the user has granted permission to Calendar
+-(void)accessGrantedForCalendar
+{
+    // Let's get the default calendar associated with our event store
+    self.defaultCalendar = self.eventStore.defaultCalendarForNewEvents;
+    // Enable the Add button
+    self.addButton.enabled = YES;
+    // Fetch all events happening in the next 24 hours and put them into eventsList
+    self.eventsList = [self fetchEvents];
+    // Update the UI with the above events
+    [self.tableView reloadData];
+}
+
+
+#pragma mark -
+#pragma mark Fetch events
+
+// Fetch all events happening in the next 24 hours
+- (NSMutableArray *)fetchEvents
+{
+    NSDate *startDate = [NSDate date];
+
+    //Create the end date components
+    NSDateComponents *tomorrowDateComponents = [[NSDateComponents alloc] init];
+    tomorrowDateComponents.day = 1;
+
+    NSDate *endDate = [[NSCalendar currentCalendar] dateByAddingComponents:tomorrowDateComponents
+                                                                    toDate:startDate
+                                                                   options:0];
+    // We will only search the default calendar for our events
+    NSArray *calendarArray = [NSArray arrayWithObject:self.defaultCalendar];
+
+    // Create the predicate
+    NSPredicate *predicate = [self.eventStore predicateForEventsWithStartDate:startDate
+                                                                      endDate:endDate
+                                                                    calendars:calendarArray];
+
+    // Fetch all events that match the predicate
+    NSMutableArray *events = [NSMutableArray arrayWithArray:[self.eventStore eventsMatchingPredicate:predicate]];
+
+    return events;
+}
+
+
+#pragma mark -
+#pragma mark Add a new event
+
+// Display an event edit view controller when the user taps the "+" button.
+// A new event is added to Calendar when the user taps the "Done" button in the above view controller.
+- (IBAction)addEvent:(id)sender
+{
+    // Create an instance of EKEventEditViewController
+    EKEventEditViewController *addController = [[EKEventEditViewController alloc] init];
+
+    // Set addController's event store to the current event store
+    addController.eventStore = self.eventStore;
+    addController.editViewDelegate = self;
+    [self presentViewController:addController animated:YES completion:nil];
+}
+
+
+#pragma mark -
+#pragma mark EKEventEditViewDelegate
+
+// Overriding EKEventEditViewDelegate method to update event store according to user actions.
+- (void)eventEditViewController:(EKEventEditViewController *)controller
+          didCompleteWithAction:(EKEventEditViewAction)action
+{
+    // Dismiss the modal view controller
+    [self dismissViewControllerAnimated:YES completion:^
+    {
+        if (action != EKEventEditViewActionCanceled)
+        {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                // Re-fetch all events happening in the next 24 hours
+                self.eventsList = [self fetchEvents];
+                // Update the UI with the above events
+                [self.tableView reloadData];
+            });
+        }
+    }];
+}
+
+
+// Set the calendar edited by EKEventEditViewController to our chosen calendar - the default calendar.
+- (EKCalendar *)eventEditViewControllerDefaultCalendarForNewEvents:(EKEventEditViewController *)controller
+{
+    return self.defaultCalendar;
 }
 
 @end
