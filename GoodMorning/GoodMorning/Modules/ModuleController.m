a557c32c5c24fbe2e2fded4219838c32db53f058
Benjamin Henry <ben@bjhenry.com>
2013-11-03 00:04:25 -0700

    Shadow boxes

--- GoodMorning/GoodMorning/Modules/ModuleController.m
+++ GoodMorning/GoodMorning/Modules/ModuleController.m
@@ -24,4 +24,17 @@
     return 1;
 }
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self addStyleToView:self.view];
+    
+}
+
+- (void)addStyleToView:(UIView *)view {
+    view.layer.shadowOffset = CGSizeMake(2, 2);
+    view.layer.shadowColor = [[UIColor grayColor] CGColor];
+    view.layer.shadowRadius = 3.0f;
+    view.layer.shadowOpacity = 0.7f;
+}
+
 @end
